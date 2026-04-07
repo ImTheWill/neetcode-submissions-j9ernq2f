cda8723928c440918db617c5a400f8c11eb4381d
William Cerritos <128171764+ImTheWill@users.noreply.github.com>
2026-04-07 09:05:55 -0700

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #string formatting look into better string manipulation 
+        s =  s.lower()
+        s = s.replace(" ", "")
+        s = s.replace("'", "")
+        s = s.replace(",", "")
+        s = s.replace("!", "")
+        s = s.replace("?", "")
+        s = s.replace(".", "")
+        s = s.replace(":", "")
+        s = s.replace(";", "")
+        
+        
+        if len(s) ==0:
+            return True
+        
+        R = len(s)-1
+        for i in range(len(s)):
+            if i > R:
+                return True
+            else:
+                if s[i] != s[R]:
+                    return False
+                else:
+                    R-=1
+        return True
+
